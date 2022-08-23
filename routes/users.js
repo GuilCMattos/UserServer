a9c3e0a0d98ad0ab937149ad1424a0649f865fd2
Guilherme <mttsguil@gmail.com>
2022-08-23 12:37:55 -0300

    add: first archives for server

---
+++ routes/users.js
@@ -0,0 +1,38 @@
+let NeDB = require('nedb');
+let db = new NeDB({
+    filename: 'users.db',
+    autoload: true
+})
+
+module.exports =  (app) => { 
+    
+    app.get('/users', (req, res) => { 
+
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json')
+        res.json({
+            users: [{
+                name: 'Guilherme',
+                email: 'mttsguil@gmail.com',
+                id: 1
+            }]
+        })
+    });
+    
+    app.post('/users', (req, res) => { 
+
+        db.insert(req.body, (err, user)=> { 
+            
+            if(err) { 
+                console.log('error', err)
+                res.status(400).json({
+                    error: err
+                });
+            } else { 
+                res.status(200).json(user)
+            }
+
+        })
+    });
+    
+}
